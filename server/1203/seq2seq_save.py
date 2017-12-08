50db392527790e3e53c60ce3487b669e87242ccd
sugspi <g1420542@is.ocha.ac.jp>
2017-12-08 16:42:09 +0900

    added

--- server/1203/seq2seq_save.py
+++ server/1203/seq2seq_save.py
@@ -14,7 +14,7 @@ import json
 import h5py
 
 batch_size = 64  # Batch size for training.
-epochs = 10  # Number of epochs to train for.
+epochs = 2  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.

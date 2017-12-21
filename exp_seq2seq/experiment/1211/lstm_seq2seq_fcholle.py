caa94f1e1fc1066a995690136a367e08739ded00
sugspi <g1420542@is.ocha.ac.jp>
2017-12-22 02:07:37 +0900

    one hot ベクトル圧縮

--- exp_seq2seq/experiment/1211/lstm_seq2seq_fcholle.py
+++ exp_seq2seq/experiment/1211/lstm_seq2seq_fcholle.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from keras.models import Model
 from keras.models import load_model
-from keras.layers import Input, LSTM, Dense
+from keras.layers import Input, LSTM,Embedding,Dense
 import keras
 from keras.utils.vis_utils import plot_model
 
@@ -12,11 +12,11 @@ import h5py
 import re
 
 batch_size = 64  # Batch size for training.
-epochs = 20  # Number of epochs to train for.
+epochs = 100  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
-data_path = '/Users/guru/MyResearch/sg/snli/json/snli_input_data_1214.json'
+data_path = '/Users/guru/MyResearch/sg/snli/json/snli_input_data_100_1214.json'
 
 
 #load dictionary
@@ -45,7 +45,6 @@ for line in jdict:
     #print(input_text)
     #print(target_text,"\n")
 
-
     for char in input_text:
         if char not in input_characters:
             input_characters.add(char)
@@ -70,48 +69,62 @@ print('Number of unique output tokens:', num_decoder_tokens)
 print('Max sequence length for inputs:', max_encoder_seq_length)
 print('Max sequence length for outputs:', max_decoder_seq_length)
 
-
 input_token_index = dict(
     [(char, i) for i, char in enumerate(input_characters)])
 target_token_index = dict(
     [(char, i) for i, char in enumerate(target_characters)])
 
-
 #np.zeros 0を要素とする配列(shape, dtype = float, order = ‘C’)　３次元
-encoder_input_data = np.zeros(
-    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
-    dtype='float32')
-decoder_input_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
-    dtype='float32')
+#encoder_input_data = np.zeros(
+#    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+#    dtype='float32')
+#decoder_input_data = np.zeros(
+#    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+#    dtype='float32')
 decoder_target_data = np.zeros(
     (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
     dtype='float32')
 
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+#    for t, char in enumerate(input_text):
+#        encoder_input_data[i, t, input_token_index[char]] = 1.
+    for t, char in enumerate(target_text):
+#        decoder_input_data[i, t, target_token_index[char]] = 1.
+       if t > 0:
+            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+
+encoder_input_data = np.zeros(
+    (len(input_texts), max_encoder_seq_length),
+    dtype='float32')
+
+decoder_input_data = np.zeros(
+    (len(target_texts), max_decoder_seq_length),
+    dtype='float32')
+
+#decoder_target_data = np.zeros(
+#    (len(target_texts), max_decoder_seq_length),
+#    dtype='float32')
+
 
 for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
     for t, char in enumerate(input_text):
-        encoder_input_data[i, t, input_token_index[char]] = 1.
+        encoder_input_data[i, t] = input_token_index[char]
     for t, char in enumerate(target_text):
-        # decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1.
-        if t > 0:
-            # decoder_target_data will be ahead by one timestep
-            # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+        decoder_input_data[i, t] = target_token_index[char]
+        #if t > 0:
+        #    decoder_target_data[i, t - 1] = target_token_index[char]
 
 # Define an input sequence and process it.
-encoder_inputs = Input(shape=(None, num_encoder_tokens))
+
+enc_main_input = Input(shape=(max_encoder_seq_length,), dtype='int32', name='enc_main_input')
+encoder_inputs  = Embedding(output_dim=512, input_dim=num_encoder_tokens, input_length=max_encoder_seq_length)(enc_main_input)
 encoder = LSTM(latent_dim, return_state=True)
-encoder_outputs, state_h, state_c = encoder(encoder_inputs)
-# We discard `encoder_outputs` and only keep the states.
+encoder_outputs, state_h, state_c  = encoder(encoder_inputs)
 encoder_states = [state_h, state_c]
 
 # Set up the decoder, using `encoder_states` as initial state.
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
-# We set up our decoder to return full output sequences,
-# and to return internal states as well. We don't use the
-# return states in the training model, but we will use them in inference.
+dec_main_input = Input(shape=(max_decoder_seq_length,), dtype='int32', name='dec_main_input')
+decoder_inputs  = Embedding(output_dim=512, input_dim=num_decoder_tokens, input_length=max_decoder_seq_length)(dec_main_input)
 decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
 decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
                                      initial_state=encoder_states)
@@ -119,7 +132,6 @@ decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
 #callback function and parameter search
-
 earlystop =keras.callbacks.EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
             #keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.0001, patience=10, verbose=0, mode='auto')
 tensorboard = keras.callbacks.TensorBoard(log_dir='logs',write_images=True,write_graph=True,)
@@ -134,22 +146,18 @@ checkpoint = keras.callbacks.ModelCheckpoint(
 # Define the model that will turn
 # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
 
-model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+model = Model([enc_main_input, dec_main_input], decoder_outputs)
 #plot_model(model, to_file='model.png')
 # Run training
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
           batch_size=batch_size,
           epochs=epochs,
-          validation_split=0.2,
-          callbacks=[tensorboard,checkpoint])
+          validation_split=0.2)
+          #callbacks=[tensorboard,checkpoint])
 
 # Save model
 model.save('s2s.h5')
-
-
-# In[6]:
-
 
 # choose input data version
  #input_texts_inf = []
@@ -173,20 +181,7 @@ model.save('s2s.h5')
 #        encoder_input_data_inf[i, t, input_token_index[char]] = 1.
 
 
-
-# In[8]:
-
-
-# Next: inference mode (sampling).
-# Here's the drill:
-# 1) encode input and retrieve initial decoder state
-# 2) run one step of decoder with this initial state
-# and a "start of sequence" token as target.
-# Output will be the next target token
-# 3) Repeat with the current target token and current states
-
 #model = load_model('s2s.h5')
-# Define sampling models
 
 #encoder_model = load_model('encoder.h5')
 encoder_model = Model(encoder_inputs, encoder_states)
@@ -214,10 +209,6 @@ reverse_input_char_index = dict(
 reverse_target_char_index = dict(
     (i, char) for char, i in target_token_index.items())
 
-
-# In[9]:
-
-
 def decode_sequence(input_seq):
     # Encode the input as state vectors.
     states_value = encoder_model.predict(input_seq)
@@ -225,7 +216,7 @@ def decode_sequence(input_seq):
     # Generate empty target sequence of length 1.
     target_seq = np.zeros((1, 1, num_decoder_tokens))
     # Populate the first character of target sequence with the start character.
-    target_seq[0, 0, target_token_index['\t']] = 1.
+    target_seq[0, 0, target_token_index['BOS']] = 1.
 
     # Sampling loop for a batch of sequences
     # (to simplify, here we assume a batch of size 1).
@@ -255,8 +246,6 @@ def decode_sequence(input_seq):
 
     return decoded_sentence
 
-
-# In[ ]:
 
 
 for seq_index in range(10):

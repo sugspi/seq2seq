2b35b0ca6bf5b2de05b2f335c08f15233d8e1841
sugspi <g1420542@is.ocha.ac.jp>
2018-01-09 15:57:00 +0900

    added emb ver, char and word

--- exp_seq2seq/experiment/1210/4th/seq2seq_char_emb_learn.py
+++ exp_seq2seq/experiment/1210/4th/seq2seq_char_emb_learn.py
@@ -7,13 +7,11 @@ from keras.layers import Input, LSTM,Embedding, Dense
 import keras
 #from keras.utils.vis_utils import load_model
 
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 import numpy as np
 import json
-import re
-import h5py
-#import pydot
+import pydot
 
 from nltk.translate.bleu_score import sentence_bleu
 
@@ -26,115 +24,117 @@ num_samples = 10000  # Number of samples to train on.
 data_path = '/Users/guru/MyResearch/sg/snli/json/snli_input_data_1214.json'
 
 # Vectorize the data.
-input_formulas = []
+input_texts = []
 target_texts = []
 output_texts =[]
-input_symbols = set()
-target_words = set()
+input_characters = set()
+target_characters = set()
 lines = open(data_path)
-
 
 jdict = json.load(lines)
 
 for line in jdict:
-    input_formula = (jdict[line])['formula']
-    input_formula = re.sub('\(', '( ',input_formula)
-    input_formula = re.sub('\)', ' )',input_formula)
-    input_formula = re.split('\s|\.', input_formula)
-    input_formula.append('EOS')
+    input_text = (jdict[line])['formula']
     target_text = (jdict[line])['text']
-    target_text = 'BOS ' + target_text + ' EOS'
-    target_text = re.split('\s|\.', target_text)
-    input_formulas.append(input_formula)
+    # We use "tab" as the "start sequence" character
+    # for the targets, and "\n" as "end sequence" character.
+    output_texts.append(target_text)
+    target_text = '\t' + target_text + '\n'
+    input_texts.append(input_text)
     target_texts.append(target_text)
+    for char in input_text:
+        if char not in input_characters:
+            input_characters.add(char)
+    for char in target_text:
+        if char not in target_characters:
+            target_characters.add(char)
 
-    for symbol in input_formula:
-        if symbol not in input_symbols:
-            input_symbols.add(symbol)
-    for word in target_text:
-        if word not in target_words:
-            target_words.add(word)
-
-input_symbols = sorted(list(input_symbols))
-target_words = sorted(list(target_words))
-num_encoder_tokens = len(input_symbols) + 1
-num_decoder_tokens = len(target_words) + 1
-max_encoder_seq_length = max([len(txt) for txt in input_formulas])
+input_characters = sorted(list(input_characters))
+target_characters = sorted(list(target_characters))
+num_encoder_tokens = len(input_characters) + 1
+num_decoder_tokens = len(target_characters) + 1
+max_encoder_seq_length = max([len(txt) for txt in input_texts])
 max_decoder_seq_length = max([len(txt) for txt in target_texts])
 
-print('Number of samples:', len(input_formulas))
+print('Number of samples:', len(input_texts))
 print('Number of unique input tokens:', num_encoder_tokens)
 print('Number of unique output tokens:', num_decoder_tokens)
 print('Max sequence length for inputs:', max_encoder_seq_length)
 print('Max sequence length for outputs:', max_decoder_seq_length)
 
 input_token_index = dict(
-    [(symbol, i+1) for i, symbol in enumerate(input_symbols)])
+    [(char, i+1) for i, char in enumerate(input_characters)])
 target_token_index = dict(
-    [(word, i+1) for i, word in enumerate(target_words)])
+    [(char, i+1) for i, char in enumerate(target_characters)])
+
 
 encoder_input_data = np.zeros(
-    (len(input_formulas), max_encoder_seq_length),
+    (len(input_texts), max_encoder_seq_length),
     dtype='float32')
 
 decoder_input_data = np.zeros(
-    (len(target_texts), max_decoder_seq_length),
+    (len(input_texts), max_decoder_seq_length),
     dtype='float32')
 
-#(1)これだけindex行列ではなくone-hotな理由がわかっていない．indexの方がよいのか？
 decoder_target_data = np.zeros(
-    (len(input_formulas), max_decoder_seq_length, num_decoder_tokens),
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
     dtype='float32')
 
-for i, (input_formula, target_text) in enumerate(zip(input_formulas, target_texts)):
-    for t, symbol in enumerate(input_formula):
-        encoder_input_data[i, t] = input_token_index[symbol]
-    for t, word in enumerate(target_text):
-        decoder_input_data[i, t] = target_token_index[word]
+for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    for t, char in enumerate(input_text):
+        encoder_input_data[i, t] = input_token_index[char]
+    for t, char in enumerate(target_text):
+        decoder_input_data[i, t] = target_token_index[char]
         if t > 0:
-            decoder_target_data[i, t - 1, target_token_index[word]] = 1.
+            # decoder_target_data will be ahead by one timestep
+            # and will not include the start character.
+            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
 
+# Define an input sequence and process it.
 enc_main_input = Input(shape=(max_encoder_seq_length,), dtype='int32', name='enc_main_input')
-encoder_inputs  = Embedding(output_dim=256, input_dim=num_encoder_tokens, input_length=max_encoder_seq_length,mask_zero=True)(enc_main_input)
+encoder_inputs  = Embedding(output_dim=70, input_dim=num_encoder_tokens, input_length=max_encoder_seq_length,mask_zero=True)(enc_main_input)
 encoder = LSTM(latent_dim, return_state=True)
 encoder_outputs, state_h, state_c  = encoder(encoder_inputs)
 encoder_states = [state_h, state_c]
 
+# Set up the decoder, using `encoder_states` as initial state.
 dec_main_input = Input(shape=(max_decoder_seq_length,), dtype='int32', name='dec_main_input')
-decoder_inputs  = Embedding(output_dim=256, input_dim=num_decoder_tokens, input_length=max_decoder_seq_length,mask_zero=True)(dec_main_input)
+decoder_inputs  = Embedding(output_dim=70, input_dim=num_decoder_tokens, input_length=max_decoder_seq_length,mask_zero=True)(dec_main_input)
 decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
 decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
                                      initial_state=encoder_states)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
 decoder_outputs = decoder_dense(decoder_outputs)
 
+
 #callback function and parameter search
+#if you want to use below function, you add callbacks=[name-val]
 earlystop =keras.callbacks.EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
-tensorboard = keras.callbacks.TensorBoard(log_dir='logs',write_images=True,write_graph=True,)
+            #keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.0001, patience=10, verbose=0, mode='auto')
+tensorboard = keras.callbacks.TensorBoard(log_dir='logs',write_images=True,write_graph=True,write_grads=True)
 checkpoint = keras.callbacks.ModelCheckpoint(
              filepath = 'elapsed_seq2seq.h5',#'seq2seq_model{epoch:02d}-loss{loss:.2f}-vloss{val_loss:.2f}.h5',
              monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
 
 # Define the model that will turn
 # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-#model = Model([enc_main_input, dec_main_input], decoder_outputs)
+model = Model([enc_main_input, dec_main_input], decoder_outputs)
 #plot_model(model, to_file='model.png')
 # Run training
-#model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-model = load_model('s2s.h5')
-m = load_model('s2s.h5')
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+m = load_model('elapsed_seq2seq.h5')
 m.save_weights('weights.h5')
 model.load_weights('weights.h5')
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_split=0.2,
-          callbacks=[checkpoint])
+#model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+#          batch_size=batch_size,
+#          epochs=epochs,
+#          validation_split=0.2,
+#          callbacks=[checkpoint]
+#          )
 
 
 # Save model
-model.save('s2s.h5')
-
+#model.save('s2s.h5')
 
 encoder_model = Model(enc_main_input, encoder_states)
 encoder_model.save('encoder.h5')
@@ -153,20 +153,20 @@ decoder_model.save('decoder.h5')
 
 # Reverse-lookup token index to decode sequences back to
 # something readable.
+reverse_input_char_index = dict(
+    (i, char) for char, i in input_token_index.items())#0 ' ',or''
 
-reverse_target_word_index = dict(
-    (i, word) for word, i in target_token_index.items())
+reverse_target_char_index = dict(
+    (i, char) for char, i in target_token_index.items())# 0:tab,1:\n
 
 def decode_sequence(input_seq):
     # Encode the input as state vectors.
     states_value = encoder_model.predict(input_seq)
 
     # Generate empty target sequence of length 1.
-    #(2)なぜ長さ１のsequenceが必要？そしてこのshapeでよいか？
     target_seq = np.zeros((1,max_decoder_seq_length))
     # Populate the first character of target sequence with the start character.
-    target_seq[0, 0] = target_token_index['BOS']
-
+    target_seq[0, 0] = target_token_index['\t']
     # Sampling loop for a batch of sequences
     # (to simplify, here we assume a batch of size 1).
     stop_condition = False
@@ -176,24 +176,21 @@ def decode_sequence(input_seq):
             [target_seq] + states_value)
 
         # Sample a token
-        #(3)なぜoutput_tokens[0, -1, :]の行をとるのか？
         sampled_token_index = np.argmax(output_tokens[0, -1, :])
-
         if sampled_token_index == 0:
             decoded_sentence += '!'
         else:
-            sampled_word = reverse_target_word_index[sampled_token_index]
-            decoded_sentence += sampled_word+' '
+            sampled_char = reverse_target_char_index[sampled_token_index]
+            decoded_sentence += sampled_char
 
         # Exit condition: either hit max length
         # or find stop character.
-        if (sampled_word == 'EOS' or
+        if (sampled_char == '\n' or
            len(decoded_sentence) > max_decoder_seq_length):
             stop_condition = True
 
         # Update the target sequence (of length 1).
         target_seq = np.zeros((1, max_decoder_seq_length))
-        #(4) (2)と同じ疑問だがtarget_seqはこのshpaeでよいのか？？
         target_seq[0, 0] = sampled_token_index
 
         # Update states
@@ -202,18 +199,18 @@ def decode_sequence(input_seq):
     return decoded_sentence
 
 #bleu evaluation
-len_inp = 10#len(input_formulas)
+len_inp = len(input_texts)
 sum_score = 0
 for seq_index in range(len_inp-1):
     input_seq = encoder_input_data[seq_index: seq_index + 1]
     decoded_sentence = decode_sequence(input_seq)
     sum_score += sentence_bleu([target_texts[seq_index]],decoded_sentence)
-    #fname = 'ccg2lambda/result'+str(seq_index)+'.txt'
-    #f = open(fname, 'w')
-    #f.write(output_texts[seq_index]+'\n')
-    #f.write(decoded_sentence.strip()+'\n')
-    #f.close()
-    #print('Input sentence:', input_formulas[seq_index])
-    print('Decoded sentence:', decoded_sentence)
-    print('Answer sentence:', target_texts[seq_index])
+    fname = 'ccg2lambda/result'+str(seq_index)+'.txt'
+    f = open(fname, 'w')
+    f.write(output_texts[seq_index]+'\n')
+    f.write(decoded_sentence.strip()+'\n')
+    f.close()
+    #print('Input sentence:', input_texts[seq_index])
+    #print('Decoded sentence:', decoded_sentence)
+    #print('Answer sentence:', target_texts[seq_index])
 print('bleu score',(sum_score/len_inp))

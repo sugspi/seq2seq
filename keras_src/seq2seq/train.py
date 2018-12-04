e63bdc50a2c88f3fdd34d982b48659b5e388641d
Kana Manome <g1420542@is.ocha.ac.jp>
2018-12-04 20:47:52 +0900

    edit train class

--- keras_src/seq2seq/train.py
+++ keras_src/seq2seq/train.py
@@ -57,16 +57,50 @@ class EncDecSequence(Sequence):
         for i, (input_text, target_text) in enumerate(zip(batch_x, batch_y)):
             decoder_mask_matrix[i] = get_masking_vector(input_text, verbose=False)
             for t, token in enumerate(input_text):
-                encoder_input_data[i, t] = corpus.formula_token_index[token]
+                encoder1_input_data[i, t] = corpus.formula_token_index[token]
             for t, token in enumerate(target_text):
                 decoder_input_data[i, t] = corpus.target_token_index[token]
                 if t > 0:
                     decoder_target_data[i, t - 1, corpus.target_token_index[token]] = 1.
 
-        return ([encoder_input_data, decoder_input_data, decoder_mask_matrix], decoder_target_data)
+        #return ([encoder_input_data, decoder_input_data, decoder_mask_matrix], decoder_target_data)
+        return ([encoder_input_data, decoder_input_data], decoder_target_data)
 
-def attention():
-    return 0
+def create_attention_model():
+    # Define an input sequence and process it.
+    enc_main_input = Input(shape=(corpus.MAX_ENCODER_SEQ_LENGTH,), dtype='int32', name='enc_main_input')
+    encoder_inputs  = Embedding(output_dim=256, input_dim=corpus.NUM_ENCODER_TOKENS, mask_zero=True, input_length=corpus.MAX_ENCODER_SEQ_LENGTH,name='enc_embedding')(enc_main_input)
+    encoder_inputs = Dropout(0.5)(encoder_inputs)
+    encoder = LSTM(latent_dim, return_state=True,return_sequences=True,name='enc_lstm', dropout=0.5, recurrent_dropout=0.5)
+    encoder_outputs, state_h, state_c  = encoder(encoder_inputs)
+    encoder_states = [state_h, state_c]
+
+    # Set up the decoder, using `encoder_states` as initial state.
+    dec_main_input = Input(shape=(corpus.MAX_DECODER_SEQ_LENGTH,), dtype='int32', name='dec_main_input')
+    decoder_inputs  = Embedding(output_dim=256, input_dim=corpus.NUM_DECODER_TOKENS, mask_zero=True, input_length=corpus.MAX_DECODER_SEQ_LENGTH,name='dec_embedding')(dec_main_input)
+    decoder_inputs  = Dropout(0.5)(decoder_inputs)
+    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True,name='dec_lstm', dropout=0.5, recurrent_dropout=0.5)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    print("dec_hidden: ",K.int_shape(decoder_outputs))
+    inner_prod = dot([encoder_outputs,decoder_outputs], axes=2)
+    print("mul (enc,hid): ",K.int_shape(inner_prod))
+    a_vector = Softmax(axis=1)(inner_prod)
+    print("a_vecotr(softmax): ",K.int_shape(a_vector))
+    context_vector = dot([a_vector,encoder_outputs], axes=1)
+    print("context_vector: ",K.int_shape(context_vector))
+    concat_vector = concatenate([context_vector,decoder_outputs], axis=2)
+    print("concat_vector: ",K.int_shape(concat_vector))
+    decoder_tanh = Dense(latent_dim, activation='tanh',name='tanh')
+    new_decoder_outputs = decoder_tanh(concat_vector)
+    new_decoder_outputs  = Dropout(0.5)(new_decoder_outputs)
+    print("new_dec_hidden: ",K.int_shape(new_decoder_outputs))
+
+    decoder_dense = Dense(corpus.NUM_DECODER_TOKENS, activation='softmax',name='softmax2')
+    new_decoder_outputs = decoder_dense(new_decoder_outputs)
+
+    model = Model([enc_main_input, dec_main_input], new_decoder_outputs)
+
+    return model
 
 if __name__ == "__main__" :
 
@@ -95,59 +129,23 @@ if __name__ == "__main__" :
     #eval.eval_blue(test_seq,model,encoder_model,decoder_model)
     ###############################################################
 
-    # Define an input sequence and process it.
-    enc_main_input = Input(shape=(corpus.MAX_ENCODER_SEQ_LENGTH,), dtype='int32', name='enc_main_input')
-    encoder_inputs  = Embedding(output_dim=256, input_dim=corpus.NUM_ENCODER_TOKENS, mask_zero=True, input_length=corpus.MAX_ENCODER_SEQ_LENGTH,name='enc_embedding')(enc_main_input)
-    encoder_inputs = Dropout(0.5)(encoder_inputs)
-    encoder = LSTM(latent_dim, return_state=True,return_sequences=True,name='enc_lstm', dropout=0.5, recurrent_dropout=0.5)
-    encoder_outputs, state_h, state_c  = encoder(encoder_inputs)
-    encoder_states = [state_h, state_c]
-
-    # Set up the decoder, using `encoder_states` as initial state.
-    dec_main_input = Input(shape=(corpus.MAX_DECODER_SEQ_LENGTH,), dtype='int32', name='dec_main_input')
-    decoder_inputs  = Embedding(output_dim=256, input_dim=corpus.NUM_DECODER_TOKENS, mask_zero=True, input_length=corpus.MAX_DECODER_SEQ_LENGTH,name='dec_embedding')(dec_main_input)
-    decoder_inputs  = Dropout(0.5)(decoder_inputs)
-    decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True,name='dec_lstm', dropout=0.5, recurrent_dropout=0.5)
-    decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
-                                         initial_state=encoder_states)
-    print("dec_hidden: ",K.int_shape(decoder_outputs))
-
-    inner_prod = dot([encoder_outputs,decoder_outputs], axes=2)
-    print("mul (enc,hid): ",K.int_shape(inner_prod))
-
-    a_vector = Softmax(axis=1)(inner_prod)
-    print("a_vecotr(softmax): ",K.int_shape(a_vector))
-
-    context_vector = dot([a_vector,encoder_outputs], axes=1)
-    print("context_vector: ",K.int_shape(context_vector))
-
-    concat_vector = concatenate([context_vector,decoder_outputs], axis=2)
-    print("concat_vector: ",K.int_shape(concat_vector))
-
-    decoder_tanh = Dense(latent_dim, activation='tanh',name='tanh')
-    new_decoder_outputs = decoder_tanh(concat_vector)
-    new_decoder_outputs  = Dropout(0.5)(new_decoder_outputs)
-    print("new_dec_hidden: ",K.int_shape(new_decoder_outputs))
-
-    decoder_dense = Dense(corpus.NUM_DECODER_TOKENS, activation='softmax',name='softmax2')
-    new_decoder_outputs = decoder_dense(new_decoder_outputs)
-
-    #mask_input = Input(shape=(corpus.MAX_DECODER_SEQ_LENGTH,corpus.NUM_DECODER_TOKENS), dtype='float32', name='mask_input')
-    #new_decoder_outputs = multiply([mask_input, new_decoder_outputs])
-
-    #callback function and parameter search
-    #if you want to use below function, you add callbacks=[name-val]
-    earlystop =keras.callbacks.EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
-                #keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.0001, patience=10, verbose=0, mode='auto')
-    #tensorboard = keras.callbacks.TensorBoard(log_dir='logs',write_images=True,write_graph=True,write_grads=True)
+    ###############################################################
+    #   callback function and parameter search
+    ###############################################################
+    earlystop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
     checkpoint = keras.callbacks.ModelCheckpoint(
                  filepath = m_path + 'elapsed_seq2seq.h5',
                  monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
 
-    ## Define the model that will turn
-    ## `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-    model = Model([enc_main_input, dec_main_input], new_decoder_outputs)
-    ## plot_model(model, to_file='model.png')
+    #tensorboard = keras.callbacks.TensorBoard(log_dir='logs',write_images=True,write_graph=True,write_grads=True)
+    # plot_model(model, to_file='model.png') #you need import pydot
+
+    ###############################################################
+    #   Setting Parameter
+    ###############################################################
+    if(corpus.model_name == 'attention'):
+        model = create_attention_model()
+
     ## Run training
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
     model.fit_generator(train_seq,
